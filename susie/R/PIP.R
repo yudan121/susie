f4550810b3e7a5412c9515bf6924cf6f3b1f4d1e
yudan121 <120547007+yudan121@users.noreply.github.com>
2022-12-15 01:46:46 +0800

    Delete susie directory

--- susie/R/PIP.R
+++
@@ -1,8 +0,0 @@
-#' Compute posterior inclusion probabilities.
-#'
-#' @param alpha_mat matrix containing posterior means of gammas.
-#' @return Posterior inclusion probabilities.
-PIP = function(alpha_mat){
-  pip = 1 - apply(1 - alpha_mat, 1, prod)
-  return(pip)
-}
